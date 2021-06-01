0cf7ba368f68ecbcc476ad716d44f571dddab3a6
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-06-01 11:52:12 +0000

    Restore default data_mem.v, but simplify led reg; use bsort

--- verilog/hardware/processor/verilog/data_mem.v
+++ verilog/hardware/processor/verilog/data_mem.v
@@ -1,43 +1,3 @@
-/*
-	Authored 2018-2019, Ryan Voo.
-
-	All rights reserved.
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions
-	are met:
-
-	*	Redistributions of source code must retain the above
-		copyright notice, this list of conditions and the following
-		disclaimer.
-
-	*	Redistributions in binary form must reproduce the above
-		copyright notice, this list of conditions and the following
-		disclaimer in the documentation and/or other materials
-		provided with the distribution.
-
-	*	Neither the name of the author nor the names of its
-		contributors may be used to endorse or promote products
-		derived from this software without specific prior written
-		permission.
-
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-	FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-	BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-	LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-	ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-	POSSIBILITY OF SUCH DAMAGE.
-*/
-
-
-
-//Data cache
-
 module data_mem (clk, addr, write_data, memwrite, memread, sign_mask, read_data, led, clk_stall);
 	input			clk;
 	input [31:0]		addr;
@@ -46,13 +6,8 @@ module data_mem (clk, addr, write_data, memwrite, memread, sign_mask, read_data,
 	input			memread;
 	input [3:0]		sign_mask;
 	output reg [31:0]	read_data;
-	output [7:0]		led;
+	output reg		led;
 	output reg		clk_stall;	//Sets the clock high
-
-	/*
-	 *	led register
-	 */
-	reg [31:0]		led_reg;
 
 	/*
 	 *	Current state
@@ -230,7 +185,7 @@ module data_mem (clk, addr, write_data, memwrite, memread, sign_mask, read_data,
 	 */
 	always @(posedge clk) begin
 		if(memwrite == 1'b1 && addr == 32'h2000) begin
-			led_reg <= write_data;
+			led <= write_data[0];
 		end
 	end
 
@@ -287,8 +242,4 @@ module data_mem (clk, addr, write_data, memwrite, memread, sign_mask, read_data,
 		endcase
 	end
 
-	/*
-	 *	Test led
-	 */
-	assign led = led_reg[7:0];
 endmodule
