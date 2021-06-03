6cb2b1c3d1863959ea1986727db0e10477ac07a7
Samuel2905 <samuelclarke2905@yahoo.co.uk>
2021-06-03 20:06:43 +0100

    added run2.m
    new run profile for benchmarks in sf

---
+++ verilog/hardware/processor/source/branch/run2.m
@@ -0,0 +1,8 @@
+newnode		riscv
+sizemem		65536
+srecl		"branch-sf.sr"
+bpt cycles 1000000
+run
+on
+ni
+dumpdistr
---
+++ verilog/hardware/processor/source/bsort/run2.m
@@ -0,0 +1,8 @@
+newnode		riscv
+sizemem		65536
+srecl		"bsort-sf.sr"
+bpt cycles 1000000
+run
+on
+ni
+dumpdistr
