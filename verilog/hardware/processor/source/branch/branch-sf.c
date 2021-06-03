553c52e8c52b92560a32ebba033e7769a3affb23
Samuel Clarke <samuelclarke2905@yahoo.co.uk>
2021-06-03 01:05:10 +0000

    branch changes

--- verilog/hardware/processor/source/branch/branch-sf.c
+++ verilog/hardware/processor/source/branch/branch-sf.c
@@ -2,19 +2,23 @@
 
 int main(void)
 {
-	int list[14] = {19, 10, 8, 17, 9, 1, 3, 6, 8, 12, 34, 5, 2, 8};
-	for (i = 1; i<13; i++) {
-		if (i == list[i]) {
-			printf("E");
-		}
-		else if (i > list[i]) {
-			printf("G");
-		}
-		else if (i < list[i]) {
-			printf("L");
-		}
-		else {
-			printf("W");
+	int list[28] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+	       	20, 21, 22, 23, 24, 25, 26, 27, 28};
+	int i, j;
+	for (i = 0; i<28; i++) {
+		for (j = 0; j<28; j++){
+			if (list[i] == list[j]) {
+				printf_("E");
+			}
+			else if (list[i] > list[j]) {
+				printf_("G");
+			}
+			else if (list[i] < list[j]) {
+				printf_("L");
+			}
+			else {
+				printf_("W");
+			}
 		}
 	}
 	return 0;
