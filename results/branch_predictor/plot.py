7bd92c620a46eceed3bdcaa93c01ec58fc6834f5
Samuel2905 <samuelclarke2905@yahoo.co.uk>
2021-06-04 18:31:11 +0100

    modified branch predictor
    Implemented 2 bit history global two level adaptive predictor (with 4 FSMs)

---
+++ results/branch_predictor/plot.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import AutoMinorLocator
+
+plt.rcParams['font.size'] = '16'
+
+supply_voltage = 1.2
+
+def plot_power(filename, figure=None):
+    if figure is None:
+        figure = plt.figure()
+    data = np.genfromtxt(filename, delimiter=",", skip_header=8)
+    plt.plot(data[:, 0], 1e3*supply_voltage*data[:, 1], label=filename)
+
+    #plt.ylim(0, 4)
+    #plt.yticks(np.arange(0, 4.5, 0.5))
+    #figure.axes[0].yaxis.set_minor_locator((AutoMinorLocator(5)))
+    plt.grid(which='major', axis='y')
+
+    plt.ylabel("Power (mW)")
+    plt.xlabel("Index")
+    plt.legend()
+    plt.title("Supply power over time for different benchmark programs")
+    plt.show()
+
+
+def plot_digital(filename, figure=None, offset=0):
+    if figure is None:
+        figure = plt.figure()
+    data = np.genfromtxt(filename, delimiter=",", skip_header=8)
+    plt.plot(data[:, 0], data[:, 1] + offset, label=filename)
+    plt.grid(axis='x')
+    plt.ylabel("Signal")
+    plt.xlabel("Time (s)")
+    plt.legend()
+    plt.title("Output signal on pin A2")
+    plt.show()
+
+
+def sorts_per_second(filename):
+    data = np.genfromtxt(filename, delimiter=",", skip_header=8)
+    time = data[:, 0]
+    signal = data[:, 1]
+    total_time = time[-1] - time[0]
+    xor = np.logical_xor(signal[:-1], signal[1:])
+    return xor.sum()/total_time
+
+
+def time_per_sort(filename):
+    return 1/sorts_per_second(filename)
+
+
+#file = "blink_24pll_A2_800hz.csv"
+file = "branch_A2_4khz.csv"
+print(sorts_per_second(file))
+print(time_per_sort(file))
+plot_digital(file)
+# plot_digital("blink_12pll_D_A2_800hz.csv")
+# plt.show()
---
+++ results/branch_predictor/plot2.py
@@ -0,0 +1,88 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.ticker import AutoMinorLocator
+
+plt.rcParams['font.size'] = '16'
+
+supply_voltage = 1.2
+
+
+def fix_fractional_seconds(fractional_seconds):
+    for i in range(len(fractional_seconds) - 1):
+        if fractional_seconds[i] > fractional_seconds[i+1]:
+            fractional_seconds[i+1:] += 1
+    fractional_seconds -= fractional_seconds[0]
+    return fractional_seconds
+
+
+def plot(powerfile, digitalfile, title, timeoffset=0):
+    if powerfile is not None:
+        power_data = np.genfromtxt(powerfile, delimiter=",", skip_header=8)
+        power = 1e3*supply_voltage*power_data[:, 1]
+        power_time = fix_fractional_seconds(power_data[:, -1])
+
+    if digitalfile is not None:
+        digital_data = np.genfromtxt(digitalfile, delimiter=",", skip_header=8)
+        value = digital_data[:, 1]
+        digital_time = digital_data[:, 0]
+        digital_time -= digital_time[0] - timeoffset
+
+    fig = plt.figure()
+    grid = fig.add_gridspec(6, 1)
+    axes = [fig.add_subplot(grid[:-1, 0]), fig.add_subplot(grid[-1, 0])]
+
+    axes[0].sharex(axes[1])
+
+    axes[0].plot(power_time, power)
+    axes[0].set_ylabel("Power consumption (mW)")
+    axes[0].grid('major', 'major')
+    axes[0].yaxis.set_minor_locator(AutoMinorLocator(5))
+
+    axes[1].plot(digital_time, value)
+    axes[1].set_xlabel("Time (s)")
+    axes[1].set_ylabel("Output")
+    axes[1].grid('major', 'major')
+    axes[1].set_xlim(0, min([digital_time[-1], power_time[-1]]))
+    axes[1].set_ylim(-0.1, 1.1)
+
+    axes[1].set_yticks([0, 1])
+    axes[1].xaxis.set_minor_locator(AutoMinorLocator(5))
+
+    axes[0].set_title(f"Power consumption and digital output ({title})")
+
+
+def avg_power(powerfile):
+    power_data = np.genfromtxt(powerfile, delimiter=",", skip_header=8)
+    power = 1e3*supply_voltage*power_data[:, 1]
+    return np.mean(power[-1000:])
+
+
+def runs(filename):
+    data = np.genfromtxt(filename, delimiter=",", skip_header=8)
+    signal = data[:, 1]
+    xor = np.logical_xor(signal[:-1], signal[1:])
+    return xor.sum()
+
+
+def runs_per_second(filename):
+    data = np.genfromtxt(filename, delimiter=",", skip_header=8)
+    time = data[:, 0][-10000:]
+    total_time = time[-1] - time[0]
+
+    signal = data[:, 1][-10000:]
+    xor = np.logical_xor(signal[:-1], signal[1:])
+
+    return xor.sum()/total_time
+
+
+def seconds_per_run(filename):
+    return 1/runs_per_second(filename)
+
+
+file = "sail_11pll_D_A2_2khz.csv"
+print(runs_per_second(file))
+print(seconds_per_run(file))
+plot("sail_11pll_D_power.csv", "sail_11pll_D_A2_2khz.csv",
+     title="factorisation of long int",
+     timeoffset=0)
+plt.show()
